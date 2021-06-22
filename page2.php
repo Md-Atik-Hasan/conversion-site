2531e2aa13a098aa68876966dbb5707c426f95ca
MD-ATIK-HASAN <atikh2767@gmail.com>
2021-06-22 10:49:43 -1200

    conversion site

---
+++ page2.php
@@ -0,0 +1,18 @@
+<html>
+<head><title>Second Page </title>  </head>
+<body>
+<fieldset>
+ <font color="blue"><h2><b>Page 2 [Conversion Rate]</b></h1></font>
+<font color="black"><h2>Conversion Site</h1></font>
+  <a  target="_blank" href="Page1.php">1.Home</a>
+ <a  target="_blank" href="Page2.php">2.Conversion Rate</a>
+ <a  target="_blank" href="Page3.php">3.History</a>
+ <br>
+ 
+ <font color="red"><h2>Conversion Rate</h1></font>
+ <input type="text" placeholder="feet to inch">  <input type="text">  <input type="text">
+ <input type="submit" value="Submit">
+ 
+ 
+ </fieldset>
+ </body>
---
+++ page3.php
@@ -0,0 +1,29 @@
+<html>
+<head><title> First Page </title>  </head>
+<body>
+<fieldset>
+ <font color="blue"><h2><b>Page 3 [History]</b></h1></font> 
+<font color="black"><h2>Conversion Site</h1></font>
+ <a href="Page1.php">1.Home</a>
+ <a href="Page2.php">2.Conversion Rate</a>
+ <a href="Page3.php">3.History</a>
+  
+ <font color="black"><h2>Conversion History</h1></font> 
+ <fieldset>
+ <table>
+ <tr>
+ <td>1.feet to inch</td>
+ <td align="center">  3</td>
+ <td align="center">  36</td>
+ </tr>
+ <tr>
+ <td>2.inch to feet</td>
+ <td align="center">  24</td>
+ <td align="center">  2</td>
+ </tr>
+ </fieldset> 
+
+</table> 
+ 
+ </fieldset>
+ </body>
